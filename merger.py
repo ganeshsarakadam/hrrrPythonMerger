0a7b6d3d75b603d03851853d765332eb8cf1e452
ganesh-srv <ganeshsrv.sarakadam@gmail.com>
2024-01-07 23:30:36 +0530

    Super sampled

--- merger.py
+++ merger.py
@@ -15,31 +15,49 @@ mergerServerApp.config['JWT_SECRET_KEY'] = 'hrrr-weather-lawn'
 jwt = JWTManager(mergerServerApp)
 
 data_folder = os.path.join(os.path.dirname(os.getcwd()), 'dataStore/now/')
-class ChunkIdFinder:
-    fs = s3fs.S3FileSystem(anon=True)
-    chunk_index = xr.open_zarr(s3fs.S3Map("s3://hrrrzarr/grid/HRRR_chunk_index.zarr", s3=fs))
+# class ChunkIdFinder:
+#     fs = s3fs.S3FileSystem(anon=True)
+#     chunk_index = xr.open_zarr(s3fs.S3Map("s3://hrrrzarr/grid/HRRR_chunk_index.zarr", s3=fs))
+
+#     @classmethod
+#     def getChunkId(cls, lat, long):
+#         projection = ccrs.LambertConformal(central_longitude=262.5, 
+#                                            central_latitude=38.5, 
+#                                            standard_parallels=(38.5, 38.5),
+#                                            globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229))
+#         x, y = projection.transform_point(long, lat, ccrs.PlateCarree())
+#         nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
+#         fcst_chunk_id = nearest_point.chunk_id.values
+#         return fcst_chunk_id, nearest_point
+
+
+# CHUNK INDEX AFTER SUPERSAMPLING TO 1KM RESOLUTION
+class ChunkIdFinderV2:
+    chunk_index = xr.open_zarr("../US-grid/HRRR_chunk_index.zarr")
+    # ds_transposed = chunk_index['chunk_id'].transpose('x', 'y')
+    # chunk_index['chunk_id']= ds_transposed
 
     @classmethod
     def getChunkId(cls, lat, long):
-        projection = ccrs.LambertConformal(central_longitude=262.5, 
-                                           central_latitude=38.5, 
-                                           standard_parallels=(38.5, 38.5),
-                                           globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229))
-        x, y = projection.transform_point(long, lat, ccrs.PlateCarree())
-        nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
-        fcst_chunk_id = nearest_point.chunk_id.values
-        return fcst_chunk_id, nearest_point
+     projection = ccrs.LambertConformal(central_longitude=262.5, 
+                                        central_latitude=38.5, 
+                                        standard_parallels=(38.5, 38.5),
+                                        globe=ccrs.Globe(semimajor_axis=6371229, semiminor_axis=6371229))
+     x, y = projection.transform_point(long, lat, ccrs.PlateCarree())
+     nearest_point = cls.chunk_index.sel(x=x, y=y, method="nearest")
+     fcst_chunk_id = nearest_point.chunk_id.values
+     return fcst_chunk_id, nearest_point
     
 
 
-@mergerServerApp.before_request
-def validate_request():
-    required_fields = ['lat', 'long', 'field', 'datetime','value']  # replace with your actual fields
-    if not request.json:
-        return jsonify({'error': 'Missing JSON in request body'}), 400
-    for field in required_fields:
-        if field not in request.json:
-            return jsonify({'error': f'Missing field: {field}'}), 400
+# @mergerServerApp.before_request
+# def validate_request():
+#     required_fields = ['lat', 'long', 'field', 'datetime','value']  # replace with your actual fields
+#     if not request.json:
+#         return jsonify({'error': 'Missing JSON in request body'}), 400
+#     for field in required_fields:
+#         if field not in request.json:
+#             return jsonify({'error': f'Missing field: {field}'}), 400
         
 
 
@@ -48,7 +66,7 @@ def hello():
     return jsonify({'status': 'Ok'})
 
 
-@jwt_required()
+# @jwt_required()
 @mergerServerApp.route('/update', methods=['PUT'])
 def update():
     data = request.json  
@@ -57,7 +75,7 @@ def update():
     field = data['field']
     datetime = data['datetime']
     value = data['value']
-    chunk_id_finder = ChunkIdFinder()
+    chunk_id_finder = ChunkIdFinderV2()
     chunk_id, nearest_point = chunk_id_finder.getChunkId(lat, lon)
     updateChunk(chunk_id,nearest_point,field,datetime,value)
     return 'Updated'
@@ -111,8 +129,8 @@ def retrieve_data_local(url):
         dtype = "<f4"
         if "surface/PRES" in url: # surface/PRES is the only variable with a larger data type
             dtype = "<f4"
-        print(dtype)
-        chunk = np.frombuffer(buffer, dtype)
+        # print(dtype)
+        chunk = np.frombuffer(buffer)
         
         entry_size = 150*150
         num_entries = len(chunk)//entry_size
